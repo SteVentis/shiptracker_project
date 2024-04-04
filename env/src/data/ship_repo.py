00acab572656faa14e1f0028be904b7af0672ab3
SteveVentis <steveventis@gmail.com>
2024-04-04 23:16:09 +0300

    changed collection name

--- env/src/data/ship_repo.py
+++ env/src/data/ship_repo.py
@@ -4,7 +4,7 @@ from models import ship
 
 def get_all_ships_details_from_db():
     db = mongodb_context.create_connection_with_db()
-    collection = db['ships_details']
+    collection = db['ships_data']
     ships_details = collection.find()
     for ship in ships_details:
         print(ship)
