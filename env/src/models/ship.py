ef62b667a3d7f51815171771e88816921897bf30
Christos8369 <161146395+Christos8369@users.noreply.github.com>
2024-04-16 21:14:40 +0300

    deleted comments

--- env/src/models/ship.py
+++ env/src/models/ship.py
@@ -1,20 +1,4 @@
 class Ship:
-    """
-    Ship class for ships with the following attributes:
-    - MMSI stands for Maritime Mobile Service Identities
-    - Name of the Vessel
-    - Heading of the vessel
-    - IMO of the vessel
-    - CallSign of the vessel
-    - Type of the vessel
-    - Status of the vessel
-    - Lenght of the vessel
-    - Width of the vessel
-    - Draft for the vessel
-    - Cargo of the vessel
-    - TranscieverClass of the vessel
-    
-    """
     def __init__(self, MMSI, VesselName, Heading, IMO, CallSign, VesselType, Status, Length, Width, Draft, Cargo, TranscieverClass):
         self.MMSI = MMSI
         self.VesselName = VesselName
@@ -28,5 +12,3 @@ class Ship:
         self.Draft = Draft
         self.Cargo = Cargo
         self.TranscieverClass = TranscieverClass
-        
-# μοντελοποίηση του αντικειμένου "πλοίο" μεσω της κλάσης Ship
